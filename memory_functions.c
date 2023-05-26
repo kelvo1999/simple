0748aeaefe4f9722f3c23e522c4d920078f6f860
MahaGaber <gabermaha2@gmail.com>
2023-05-26 10:49:38 -0700

    add

--- memory_functions.c
+++ memory_functions.c
@@ -60,3 +60,20 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	return (p);
 }
 
+
+/**
+ * bfree - frees a pointer
+ * @ptr: address of the pointer to free
+ * Return: 1 , 0.
+ */
+int bfree(void **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
+
